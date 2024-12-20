7e11b291e512c16783142d3b59008c8b74b37bcb
ali <ali_teimoury1@yahoo.com>
2024-12-20 11:51:39 -0500

    second commit

--- main.go
+++ main.go
@@ -9,43 +9,49 @@ import (
 )
 
 func main() {
-	for {
-		makeConnection("20241")
-		makeConnection("20242")
-		makeConnection("3309")
-	}
+	go makeConnection("20241", "8001")
+	go makeConnection("20242", "8002")
+	go makeConnection("3309", "8009")
+
+	select {}
 }
 
-func makeConnection(port string) {
-	messageConn, err := net.Dial("tcp", "localhost:"+port)
-	if err != nil {
-		log.Fatalf("Failed to connect to message port: %v", err)
+func makeConnection(local_port, remote_port string) {
+	for {
+		tcpConn, err := net.Dial("tcp", "localhost:"+local_port)
+		if err != nil {
+			log.Fatalf("Failed to connect to message local_port: %v", err)
+		}
+
+		// Establish WebSocket connection to VPS
+		vpsURL := url.URL{Scheme: "ws", Host: "192.168.1.104", Path: "/admin"}
+		query := vpsURL.Query()
+		query.Set("port", remote_port)
+		vpsURL.RawQuery = query.Encode()
+
+		ws, _, err := websocket.DefaultDialer.Dial(vpsURL.String(), nil)
+		if err != nil {
+			log.Fatalf("Failed to connect to VPS: %v", err)
+		}
+
+		//read RND and ORG from server and send it to admin
+		_, message, err := ws.ReadMessage()
+		if err != nil {
+			log.Printf("WebSocket connection closed: %v", err)
+			return
+		}
+
+		_, err = tcpConn.Write(message)
+		if err != nil {
+			log.Printf("Failed to write data to TCP: %v", err)
+			return
+		}
+
+		fmt.Println(message)
+
+		// Start proxying connections
+		go handleConnection(tcpConn, ws)
 	}
-
-	// Establish WebSocket connection to VPS
-	vpsURL := url.URL{Scheme: "wss", Host: "vps.example.com", Path: "/admin"}
-	ws, _, err := websocket.DefaultDialer.Dial(vpsURL.String(), nil)
-	if err != nil {
-		log.Fatalf("Failed to connect to VPS: %v", err)
-	}
-
-	// Send KEY for identification
-	key := "1234-5678" // Replace with actual key logic
-	err = ws.WriteMessage(websocket.TextMessage, []byte(key))
-	if err != nil {
-		log.Fatalf("Failed to send KEY: %v", err)
-	}
-
-	_, message, err := ws.ReadMessage()
-	if err != nil {
-		log.Printf("WebSocket connection closed: %v", err)
-		return
-	}
-
-	fmt.Println(message)
-
-	// Start proxying connections
-	go handleConnection(messageConn, ws)
 }
 
 func handleConnection(tcpConn net.Conn, ws *websocket.Conn) {
