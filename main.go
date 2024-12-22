5ea3780f0a1b9c04658dab2b87463b847ede7c62
ali <ali_teimoury1@yahoo.com>
2024-12-22 03:43:59 -0500

    fixed problems

--- main.go
+++ main.go
@@ -8,48 +8,73 @@ import (
 	"net/url"
 )
 
+var server = "192.168.1.105"
+var address = "/websocket"
+var token = "1234"
+var org = "4567"
+
 func main() {
-	go makeConnection("20241", "8001")
-	go makeConnection("20242", "8002")
-	go makeConnection("3309", "8009")
+	go makeConnection("20241")
+	go makeConnection("20242")
+	go makeConnection("3309")
 
 	select {}
 }
 
-func makeConnection(local_port, remote_port string) {
+func makeConnection(local_port string) {
 	for {
-		tcpConn, err := net.Dial("tcp", "localhost:"+local_port)
-		if err != nil {
-			log.Fatalf("Failed to connect to message local_port: %v", err)
-		}
-
 		// Establish WebSocket connection to VPS
-		vpsURL := url.URL{Scheme: "ws", Host: "192.168.1.104", Path: "/admin"}
+		vpsURL := url.URL{Scheme: "ws", Host: server + ":9090", Path: address + "/admin"}
 		query := vpsURL.Query()
-		query.Set("port", remote_port)
+		query.Set("port", local_port)
 		vpsURL.RawQuery = query.Encode()
 
 		ws, _, err := websocket.DefaultDialer.Dial(vpsURL.String(), nil)
-		if err != nil {
-			log.Fatalf("Failed to connect to VPS: %v", err)
+		if err != nil || ws == nil {
+			log.Printf("Failed to connect to VPS: %v", err)
+			continue
 		}
+
+		fmt.Println("Connected WEB Port:", local_port)
+
+		err = ws.WriteMessage(websocket.BinaryMessage, []byte(`{"Token":"`+token+`","ORG":"`+org+`"}`))
+		if err != nil {
+			log.Printf("Failed to send handshake: %v", err)
+			ws.Close()
+			continue
+		}
+
+		fmt.Println("Sent WEB Port:", local_port)
 
 		//read RND and ORG from server and send it to admin
 		_, message, err := ws.ReadMessage()
 		if err != nil {
 			log.Printf("WebSocket connection closed: %v", err)
-			return
+			ws.Close()
+			continue
 		}
 
-		_, err = tcpConn.Write(message)
+		fmt.Println("Read WEB Port:", local_port)
+
+		tcpConn, err := net.Dial("tcp", "localhost:"+local_port)
 		if err != nil {
-			log.Printf("Failed to write data to TCP: %v", err)
-			return
+			log.Printf("Failed to connect to message local_port: %v", err)
+		}
+
+		fmt.Println("Connected TCP Port:", local_port)
+
+		if local_port != "3309" {
+			_, err = tcpConn.Write(message)
+			if err != nil {
+				log.Printf("Failed to write data to TCP: %v", err)
+				ws.Close()
+				tcpConn.Close()
+				continue
+			}
 		}
 
 		fmt.Println(message)
 
-		// Start proxying connections
 		go handleConnection(tcpConn, ws)
 	}
 }
@@ -60,6 +85,9 @@ func handleConnection(tcpConn net.Conn, ws *websocket.Conn) {
 
 	// Goroutine to forward data from TCP to WebSocket
 	go func() {
+		defer tcpConn.Close()
+		defer ws.Close()
+
 		buffer := make([]byte, 32*1024)
 		for {
 			// Read from TCP connection
