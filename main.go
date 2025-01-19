0859baecb0190a6e1511afc8daac1f4e9a1ffb6a
ali <ali_teimoury1@yahoo.com>
2025-01-19 04:15:48 -0500

    read first message fixed bugs

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/gorilla/websocket"
@@ -11,11 +12,14 @@ import (
 	"time"
 )
 
-// var server = "192.168.1.104:9090"
-var server = "expanel.app"
+//var server = "192.168.1.105:9090"
+
+var server = "mail.expanel.app:9090"
+
 var address = "/websocket"
 
-// var wss = "ws"
+//var wss = "ws"
+
 var wss = "wss"
 
 var token *string
@@ -48,7 +52,7 @@ func makeConnection(local_port string) {
 		query.Set("port", local_port)
 		vpsURL.RawQuery = query.Encode()
 
-		fmt.Println("Try Connecting WEB Port:", local_port)
+		fmt.Println("Try Connecting WEB : ", vpsURL.String())
 
 		ws, _, err := websocket.DefaultDialer.Dial(vpsURL.String(), nil)
 		if err != nil || ws == nil {
@@ -57,7 +61,7 @@ func makeConnection(local_port string) {
 			continue
 		}
 
-		fmt.Println("Connected WEB Port:", local_port)
+		fmt.Println("Connected WEB Port: ", local_port)
 
 		err = ws.WriteMessage(websocket.BinaryMessage, []byte(`{"Token":"`+*token+`","ORG":"`+*org+`"}`))
 		if err != nil {
@@ -67,12 +71,33 @@ func makeConnection(local_port string) {
 			continue
 		}
 
-		fmt.Println("Sent WEB Port:", local_port)
+		fmt.Println("Sent WEB Port: ", local_port)
+
+		if local_port != "3309" {
+			//read start communication to make sure connection is alive
+			fmt.Println("Read start message")
+			_, message, err3 := ws.ReadMessage()
+			if message != nil {
+				fmt.Printf("First message is: %s \n", string(message))
+			}
+			if err3 != nil {
+				fmt.Printf("Error read first message %v\n", err3)
+				ws.Close()
+				time.Sleep(3 * time.Second)
+				continue
+			}
+		}
 
 		//read RND and ORG from server and send it to admin
-		_, message, err := ws.ReadMessage()
+		n, message, err := ws.ReadMessage()
 		if err != nil {
-			fmt.Printf("WebSocket connection closed: %v\n", err)
+			if websocket.IsCloseError(err, websocket.CloseNormalClosure, websocket.CloseGoingAway) {
+				fmt.Printf("WebSocket closed normally: %v\n", err)
+			} else if websocket.IsUnexpectedCloseError(err) {
+				fmt.Printf("Unexpected WebSocket error: %v\n", err)
+			} else {
+				fmt.Printf("Other error: %v\n", err)
+			}
 			ws.Close()
 			time.Sleep(3 * time.Second)
 			continue
@@ -80,9 +105,12 @@ func makeConnection(local_port string) {
 
 		fmt.Println("Read WEB Port:", local_port)
 
-		tcpConn, err := net.Dial("tcp", "localhost:"+local_port)
+		tcpConn, err := net.Dial("tcp", "localhost: "+local_port)
 		if err != nil {
 			fmt.Printf("Failed to connect to message local_port: %v\n", err)
+			ws.Close()
+			time.Sleep(3 * time.Second)
+			continue
 		}
 
 		fmt.Println("Connected TCP Port:", local_port)
@@ -96,17 +124,18 @@ func makeConnection(local_port string) {
 				time.Sleep(3 * time.Second)
 				continue
 			}
+		} else {
+			fmt.Printf("First message Ports: %s %s size: %d \n", local_port, string(message), n)
 		}
 
-		fmt.Println(message)
-
-		go handleConnection(tcpConn, ws)
+		go handleConnection(tcpConn, ws, local_port)
 	}
 }
 
-func handleConnection(tcpConn net.Conn, ws *websocket.Conn) {
+func handleConnection(tcpConn net.Conn, ws *websocket.Conn, port string) {
 	defer tcpConn.Close()
 	defer ws.Close()
+	defer log.Printf("Proxy communication finished %s", port)
 
 	// Goroutine to forward data from TCP to WebSocket
 	go func() {
@@ -119,15 +148,20 @@ func handleConnection(tcpConn net.Conn, ws *websocket.Conn) {
 			n, err := tcpConn.Read(buffer)
 			if n > 0 {
 				// Forward to WebSocket
-				err = ws.WriteMessage(websocket.BinaryMessage, buffer[:n])
-				if err != nil {
-					fmt.Printf("Failed to send data to WebSocket: %v\n", err)
+				err2 := ws.WriteMessage(websocket.BinaryMessage, buffer[:n])
+				if err2 != nil {
+					fmt.Printf("Failed to send data to WebSocket: %v\n", err2)
 					return
 				}
 			}
 			if err != nil {
-				fmt.Printf("TCP connection closed: %v\n", err)
-				return
+				if errors.Is(err, net.ErrClosed) {
+					fmt.Println("TCP connection closed by peer")
+					return
+				} else {
+					fmt.Printf("TCP connection read error: %v\n", err)
+					return
+				}
 			}
 		}
 	}()
@@ -138,15 +172,20 @@ func handleConnection(tcpConn net.Conn, ws *websocket.Conn) {
 		_, message, err := ws.ReadMessage()
 		if message != nil {
 			// Write to TCP connection
-			_, err = tcpConn.Write(message)
-			if err != nil {
-				fmt.Printf("Failed to write data to TCP: %v\n", err)
+			_, err2 := tcpConn.Write(message)
+			if err2 != nil {
+				fmt.Printf("Failed to write data to TCP: %v\n", err2)
 				return
 			}
 		}
 		if err != nil {
-			fmt.Printf("WebSocket connection closed: %v\n", err)
-			return
+			if errors.Is(err, net.ErrClosed) {
+				fmt.Println("TCP connection closed by peer")
+				return
+			} else {
+				fmt.Printf("WS connection read error: %v\n", err)
+				return
+			}
 		}
 	}
 }
